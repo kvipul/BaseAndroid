4a096d43dd438d5b52e2ddb37a9a0717dac60d89
kvipul <vpul01@gmail.com>
2020-07-18 20:08:38 +0530

    BaseFragment.kt and BaseViewModel.kt added

--- BaseAndroidLibrary/src/main/java/com/sablania/baseandroidlibrary/BaseActivity.kt
+++ BaseAndroidLibrary/src/main/java/com/sablania/baseandroidlibrary/BaseActivity.kt
@@ -7,9 +7,12 @@ import androidx.appcompat.app.AppCompatActivity
 /**
  * @author Vipul Kumar on 18/07/20.
  */
-open class BaseActivity: AppCompatActivity() {
+open class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
+
     }
+
+    open fun getActivityName() = this::class.java.simpleName
 }
---
+++ BaseAndroidLibrary/src/main/java/com/sablania/baseandroidlibrary/BaseViewModel.kt
@@ -0,0 +1,11 @@
+package com.sablania.baseandroidlibrary
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+/**
+ * Base ViewModel class for all the ViewModels
+ */
+open class BaseViewModel(application: Application) : AndroidViewModel(application) {
+
+}
