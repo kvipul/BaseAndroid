6fcd8fe9559e3f3a075736e99d590214b1367218
kvipul <vpul01@gmail.com>
2020-07-18 18:55:49 +0530

    Base activity added

---
+++ BaseAndroidLibrary/src/main/java/com/sablania/baseandroidlibrary/BaseActivity.kt
@@ -0,0 +1,15 @@
+package com.sablania.baseandroidlibrary
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @author Vipul Kumar on 18/07/20.
+ */
+open class BaseActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+}
