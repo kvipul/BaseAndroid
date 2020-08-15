e6aa92153a5a6c6087e67009867c262cfaf2021c
kvipul <vpul01@gmail.com>
2020-08-15 22:15:17 +0530

    Firebase Crashlytics integrated

--- BaseAndroidLibrary/src/main/java/com/sablania/baseandroidlibrary/BaseAndroidLibrary.kt
+++ BaseAndroidLibrary/src/main/java/com/sablania/baseandroidlibrary/BaseAndroidLibrary.kt
@@ -1,12 +1,14 @@
 package com.sablania.baseandroidlibrary
 
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+
 /**
  * @author Vipul Kumar on 15/08/20.
  */
 object BaseAndroidLibrary {
-//    internal lateinit var firebaseCrashlytics: FirebaseCrashlytics
-//
-//    fun setUpLibrary(firebaseCrashlytics: FirebaseCrashlytics) {
-//        this.firebaseCrashlytics = firebaseCrashlytics
-//    }
+    internal lateinit var firebaseCrashlytics: FirebaseCrashlytics
+
+    fun setUpLibrary(firebaseCrashlytics: FirebaseCrashlytics) {
+        this.firebaseCrashlytics = firebaseCrashlytics
+    }
 }
--- BaseAndroidLibrary/src/main/java/com/sablania/baseandroidlibrary/MyLog.kt
+++ BaseAndroidLibrary/src/main/java/com/sablania/baseandroidlibrary/MyLog.kt
@@ -1,6 +1,10 @@
 package com.sablania.baseandroidlibrary
 
+import android.content.Context
+import android.os.Bundle
 import android.util.Log
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.sablania.baseandroidlibrary.BaseAndroidLibrary.firebaseCrashlytics
 
 /**
  * @author Vipul Kumar on 15/08/20.
@@ -8,29 +12,28 @@ import android.util.Log
 
 class MyLog {
     companion object {
-
         fun i(tag: String?, msg: String) {
             Log.i((tag ?: "").plus("Sablania"), msg)
-//            firebaseCrashlytics.log((tag ?: "").plus(" ").plus(msg))
+            firebaseCrashlytics.log((tag ?: "").plus(" ").plus(msg))
         }
 
         fun w(tag: String?, msg: String) {
             Log.w((tag ?: "").plus("Sablania"), msg)
-//            firebaseCrashlytics.log((tag ?: "").plus(" ").plus(msg))
+            firebaseCrashlytics.log((tag ?: "").plus(" ").plus(msg))
         }
 
         fun e(tag: String?, msg: String) {
             Log.e((tag ?: "").plus("Sablania"), msg)
-//            firebaseCrashlytics.log((tag ?: "").plus(" ").plus(msg))
+            firebaseCrashlytics.log((tag ?: "").plus(" ").plus(msg))
         }
     }
 }
 
-//fun logEvent(context: Context, eventName: String, bundle: Bundle?) {
-//    FirebaseAnalytics.getInstance(context).logEvent(eventName, bundle)
-//}
+fun logEvent(context: Context, eventName: String, bundle: Bundle?) {
+    FirebaseAnalytics.getInstance(context).logEvent(eventName, bundle)
+}
 
 fun Exception.logStackTrace() {
     printStackTrace()
-//    firebaseCrashlytics.recordException(this)
+    firebaseCrashlytics.recordException(this)
 }
